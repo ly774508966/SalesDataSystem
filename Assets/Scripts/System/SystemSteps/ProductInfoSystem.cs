5520ff5499d09618a56604646ba3451599fe39bb
harryloGrace <1159040290@qq.com>
2018-09-20 21:07:09 +0800

    产品信息界面
    使用ScrollView完成产品信息的显示

--- Assets/Scripts/System/SystemSteps/ProductInfoSystem.cs
+++ Assets/Scripts/System/SystemSteps/ProductInfoSystem.cs
@@ -4,9 +4,56 @@ using UnityEngine;
 
 public class ProductInfoSystem : SystemStepBase
 {
+    /// <summary>
+    /// 产品信息UI父物体
+    /// </summary>
+    public Transform ProductInfoContent;
+    /// <summary>
+    /// 产品信息的预制
+    /// </summary>
+    public GameObject ProductInfoPrefab;
+
+    private List<ProductInfoPrefabInfo> productInfoPrefabList = new List<ProductInfoPrefabInfo>();
+
     public void OnEnable()
     {
-        
+        UpdateProductShow();
     }
 
+    private void UpdateProductShow()
+    {
+        List<ProductInfo> productInfos = new List<ProductInfo>();
+        foreach (var p in ConfigDataManager.ProductInfosDict)
+        {
+            productInfos.Add(p.Value);
+        }
+
+        int curPrefabIndex = 0;
+        for (int i = 0; i < productInfos.Count; i++)
+        {
+            curPrefabIndex++;
+            if (curPrefabIndex > productInfoPrefabList.Count)
+            {
+                CreateProductPrefab(productInfos[i]);
+            }
+        }
+        if (curPrefabIndex < productInfoPrefabList.Count)
+        {
+            for (int i = curPrefabIndex; i < productInfoPrefabList.Count; i++)
+            {
+                productInfoPrefabList[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void CreateProductPrefab(ProductInfo info)
+    {
+        GameObject newGo = GameObject.Instantiate(ProductInfoPrefab);
+        newGo.SetActive(false);
+        newGo.transform.SetParent(ProductInfoContent);
+        ProductInfoPrefabInfo prefabInfo = newGo.GetComponent<ProductInfoPrefabInfo>();
+        prefabInfo.SetValue(info);
+        productInfoPrefabList.Add(prefabInfo);
+        newGo.SetActive(true);
+    }
 }
---
+++ Assets/Scripts/UI/ProductInfoPrefabInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProductInfoPrefabInfo : MonoBehaviour
+{
+    public Text Text_ID;
+    public Text Text_Name;
+    public Text Text_UnitPrice;
+
+    public void SetValue(ProductInfo productinfo)
+    {
+        Text_ID.text = productinfo.ProductID.ToString();
+        Text_Name.text = productinfo.ProductName;
+        Text_UnitPrice.text = productinfo.UnitPrice.ToString();
+    }
+}
